bb665b50f9993bab123f63b8d191df4c59b49133
Nilson Flávio Pereira <nilsonfpereira@hotmail.com>
2023-01-27 10:00:41 -0300

    Remoção dos pacotes react-router-dom e react-icons gerado fora do projeto portifolio

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Projetos from './pages/Projetos';
+import Curriculum from './pages/Curriculum';
+
 const App = () => {
   return (
-    <div className='App'>App</div>
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/projetos">Projetos</Link>
+      <Link to="/curriculum">Curriculum</Link>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/projetos' element={<Projetos />} />
+        <Route path='/curriculum' element={<Curriculum/>}  />
+      </Routes>
+    </Router>
   )
 }
 
