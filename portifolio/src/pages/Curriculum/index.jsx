4f74bb5a2d55678d18accc8490681a76ca3a572a
Nilson Flávio Pereira <nilsonfpereira@hotmail.com>
2023-01-27 09:47:43 -0300

    Inclusão do pacote react-router-dom e react-icons e criação de páginas

---
+++ portifolio/src/pages/Curriculum/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Curriculum = () => {
+  return (
+    <div>Curriculum</div>
+  )
+}
+
+export default Curriculum;
---
+++ portifolio/src/pages/Projetos/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Projetos = () => {
+  return (
+    <div>Projetos</div>
+  )
+}
+
+export default Projetos;
